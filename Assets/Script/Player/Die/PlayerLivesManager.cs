b9d7312fbe5d316ce3751bc02440322703cb10d9
Ali Fakhar <gbawag186@gmail.com>
2025-09-22 22:37:10 +0500

    Clear UI Buttons After Ninja Death

--- Assets/Script/Player/Die/PlayerLivesManager.cs
+++ Assets/Script/Player/Die/PlayerLivesManager.cs
@@ -8,18 +8,29 @@ public class PlayerLivesManager : MonoBehaviour
     public int totalLives = 3;
     public GameObject player;
     public GameObject deathUI;
-    public GameObject playerUI; // Drag your HUD Canvas (playerUI) here in Inspector
+    public GameObject playerUI;
+    public GameObject settingUI;
     public CinemachineCamera virtualCamera;
 
     private int deathCount = 0;
     private Vector3 respawnPoint;
-    private float deathUIDelay = 3f; // Delay in seconds before showing death UI
+    private float deathUIDelay = 3f; 
 
     private void Start()
     {
         respawnPoint = player.transform.position;
         deathUI.SetActive(false);
         UIManager.Instance.UpdateLivesUI(totalLives - deathCount);
+
+        if(settingUI != null)
+        {
+            settingUI.SetActive(true);
+        }
+        if (playerUI != null)
+        {
+            playerUI.SetActive(true);
+        }
+
     }
 
     public void UpdateRespawnPoint(Vector3 newPoint)
@@ -46,10 +57,11 @@ public class PlayerLivesManager : MonoBehaviour
             {
                 playerUI.SetActive(false);
             }
-            else
+            if(settingUI != null)
             {
-                Debug.LogWarning("Player UI not assigned in PlayerLivesManager!");
+                settingUI.SetActive(false);
             }
+
 
             StartCoroutine(ShowDeathUIWithDelay());
         }
@@ -58,6 +70,7 @@ public class PlayerLivesManager : MonoBehaviour
     private IEnumerator ShowDeathUIWithDelay()
     {
         yield return new WaitForSeconds(deathUIDelay);
+
         deathUI.SetActive(true);
     }
 
@@ -65,6 +78,16 @@ public class PlayerLivesManager : MonoBehaviour
     {
         deathCount = 0;
         UIManager.Instance.UpdateLivesUI(totalLives);
+
+        if(settingUI != null)
+        {
+            settingUI.SetActive(true);
+        }
+        if (playerUI != null)
+        {
+            playerUI.SetActive(true);
+        }
+            
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
